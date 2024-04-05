79104a9323805b079c432e89589937b316406f44
Kendalite <bastien.quenet.pro@gmail.com>
2024-04-05 17:06:49 +0200

    initFavIconAndRobots

---
+++ resources/views/layouts/favicon.blade.php
@@ -0,0 +1,9 @@
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
+<link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}">
+<link rel="mask-icon" href="{{ asset('favicon/safari-pinned-tab.svg') }}" color="#fcd43f">
+<meta name="msapplication-TileColor" content="#086862">
+<meta name="msapplication-TileColor" content="#ffc40d">
+<meta name="msapplication-TileImage" content="{{ asset('favicon/mstile-144x144.png') }}">
+<meta name="theme-color" content="#fcd43f">
