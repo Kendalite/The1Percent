8b540d0e99240571a8061e015d41e7544438cb3f
Kendalite <bastien.quenet.pro@gmail.com>
2024-05-19 17:31:14 +0200

    textSize_Redo

--- resources/views/game/play.blade.php
+++ resources/views/game/play.blade.php
@@ -6,14 +6,14 @@
                 <p class="text-gold text-2xl md:text-4xl font-bold text-center w-full flex items-baseline justify-center"><span class="text-4xl md:text-7xl">100</span>%</p>
             </aside>
             <h1
-                class="text-sm md:text-2xl lg:text-4xl text-white text-center font-bold font-sans bg-black border-taupe border-4 rounded-xl pl-16 md:pl-32 pr-4 py-6 mx-auto w-full shadow-lg shadow-black ml-12 md:ml-24 z-20">
-                What shape is represented in this example ?
+                class="text-sm md:text-xl lg:text-2xl text-white text-center font-bold font-sans bg-black border-taupe border-4 rounded-xl pl-16 md:pl-32 pr-4 py-2 lg:py-6 mx-auto w-full shadow-lg shadow-black ml-12 md:ml-24 z-20">
+                There are 3 cards on the table : The Ace of Spades, Ace of Hearts and Ace of Clubs. Knowing the heart is not next to the club and the heart is not in position C, where is the Ace of Space ?
             </h1>
             <aside class="absolute flex rounded-2xl w-32 bg-dark items-end justify-center border-gold border-4 left-12 max-sm:left-0 md:left-48 lg:left-96 right-auto py-0 pt-4 -top-8 lg:-top-10 z-10">
                 <p class="text-white text-2xl">Score</p>
             </aside>
             <aside class="absolute flex rounded-2xl max-sm:w-fit w-96 bg-dark items-end justify-center border-gold border-4 left-auto right-0 py-0 pt-4 -top-8 lg:-top-10 z-10">
-                <p class="text-white text-xs max-sm:px-4 max-sm:py-2 md:text-2xl">ThisIsAVeryLongUsername</p>
+                <p class="text-white text-xs max-sm:px-4 max-sm:py-2 sm:text-2xl">ThisIsAVeryLongUsername</p>
             </aside>
         </div>
         <section class="w-full h-full system-dynamic-height flex-initial inline-flex max-w-screen-2xl bg-black border-taupe border-4 rounded-xl items-center justify-center">
